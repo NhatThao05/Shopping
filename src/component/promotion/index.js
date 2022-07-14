a5436e9f455c17566ad64e74965df987c84c7a9c
Thao <levunhatthao05@gmail.com>
2022-07-14 13:01:13 +0300

    Promotion commit

--- src/component/promotion/index.js
+++ src/component/promotion/index.js
@@ -1,4 +1,7 @@
-import { PromotionContainer } from "../../styles/promotion"
+import { MessageText, MyList, PromotionContainer, SellingContainer } from "../../styles/promotion"
+import { Typography, Slide } from "@mui/material";
+import { useEffect, useState, useRef } from "react";
+import {Box} from "@mui/system"
 
 const messages = [
  "Free delivery from 2.8 million",
@@ -7,10 +10,46 @@ const messages = [
 ]
 
 export default function Promotion(){
- return (
-  <PromotionContainer>
-   <h1>Promotion component</h1>
+     const containerRef = useRef();
+     const [index, setIndex] = useState(0);
+     const [show, setShow] = useState(true);
+     useEffect(() => {
+          setTimeout(() => {
+               setShow(false)
+          },3000);
 
+          const intervalID = setInterval(() => {
+               setIndex((i) => (i + 1) % messages.length);
+               setShow(true);
+
+               setTimeout(() => {
+                    setShow(false)
+               },3000)
+          },4000);
+          return() => {
+               clearInterval(intervalID)
+          }
+     },[])
+ return (
+<>
+  <SellingContainer>
+   <MyList type="row">
+        <Typography variant="h6" sx={{fontWeight: 'bolder', mr: 4}}>Nutrition</Typography>
+        <Typography variant="h6" sx={{fontWeight: 'bolder', mr: 4}}>Clothing</Typography>
+        <Typography variant="h6" sx={{fontWeight: 'bolder', mr: 4}}>Products</Typography>
+        <Typography variant="h6" sx={{fontWeight: 'bolder', mr: 4}}>Food</Typography>
+        <Typography variant="h6" sx={{fontWeight: 'bolder', mr: 4}}>Fitness & Health Goals</Typography>
+        <Typography variant="h6" sx={{fontWeight: 'bolder'}}>More Information</Typography> 
+   </MyList>
+  </SellingContainer>
+  <PromotionContainer ref={containerRef}>
+     <Slide container={containerRef.current} direction={show ? "left" : "right"} in={show} timeout={{enter: 500, exit: 300}}>
+          <Box display={"flex"} justifyContent="center" alignItems={"center"}>
+               <MessageText>{messages[index]}</MessageText>
+          </Box>
+     </Slide> 
   </PromotionContainer>
+</>
+
  )
 }
--- src/styles/promotion/index.js
+++ src/styles/promotion/index.js
@@ -1,11 +1,45 @@
+import { Typography } from "@mui/material"
 import {Box, styled} from "@mui/system"
 import { Colors } from "../theme"
 
-export const PromotionContainer = styled(Box)(() => ({
+export const SellingContainer = styled(Box)(({theme}) => ({
+ [theme.breakpoints.up('md')]: {
+  padding: '20px 0px 20px 0px'
+ },
  display: 'flex',
  justifyContent: 'center',
  alignItems: 'center',
- padding: '10px 5px 10px 5px',
+ padding: '10px 0px 10px 0px',
  background: Colors.primary,
- color: Colors.light_gray
+ color: Colors.light_gray,
 }))
+
+export const MyList = styled(Typography)(({type}) => ({
+ display: type === 'row' ? 'flex' : 'block',
+ flexGrow: 3,
+ justifyContent: 'center',
+ alignItems: "center",
+}))
+
+export const PromotionContainer = styled(Box)(({theme}) => ({
+ [theme.breakpoints.up('md')]: {
+  padding: '20px 0px 20px 0px'
+ },
+ display: 'flex',
+ justifyContent: 'center',
+ alignItems: 'center',
+ padding: '10px 0px 10px 0px',
+ background: "#E3F2FD",
+ color: Colors.primary,
+ overflow: 'hidden',
+}))
+
+export const MessageText = styled(Typography)(({theme}) => ({
+ [theme.breakpoints.up('md')]: {
+  fontSize: '1.5rem',
+ },
+ color: Colors.primary,
+ fontSize: '1rem'
+}))
+
+
